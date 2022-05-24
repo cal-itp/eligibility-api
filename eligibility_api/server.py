4126dcf43dcc5de22ffb6c738de76e176c21f8e4
Angela Tran <angela@compiler.la>
2022-05-24 20:32:49 +0000

    refactor: update server functions to also use new JWK class

--- eligibility_api/server.py
+++ eligibility_api/server.py
@@ -2,7 +2,9 @@ import datetime
 import json
 import logging
 
-from jwcrypto import jwe, jws, jwk, jwt
+from jwcrypto import jwe, jws, jwt
+
+from .client import JWK
 
 logger = logging.getLogger(__name__)
 
@@ -11,20 +13,20 @@ def get_token_payload(
     token: str,
     jwe_encryption_alg: str,
     jwe_cek_enc: str,
-    server_private_key: jwk.JWK,
+    server_private_key: JWK,
     jws_signing_alg: str,
-    client_public_key: jwk.JWK,
+    client_public_key: JWK,
 ) -> dict:
     """Decode a token (JWE(JWS))."""
     try:
         # decrypt
         decrypted_token = jwe.JWE(algs=[jwe_encryption_alg, jwe_cek_enc])
-        decrypted_token.deserialize(token, key=server_private_key)
+        decrypted_token.deserialize(token, key=server_private_key.key)
         decrypted_payload = str(decrypted_token.payload, "utf-8")
         # verify signature
         signed_token = jws.JWS()
         signed_token.deserialize(
-            decrypted_payload, key=client_public_key, alg=jws_signing_alg
+            decrypted_payload, key=client_public_key.key, alg=jws_signing_alg
         )
         # return final payload
         payload = str(signed_token.payload, "utf-8")
@@ -50,16 +52,16 @@ def create_response_payload(token_payload: dict, issuer: str) -> dict:
 def make_token(
     payload: dict,
     jws_signing_alg: str,
-    server_private_key: jwk.JWK,
+    server_private_key: JWK,
     jwe_encryption_alg: str,
     jwe_cek_enc: str,
-    client_public_key: jwk.JWK,
+    client_public_key: JWK,
 ) -> str:
     """Wrap payload in a signed and encrypted JWT for response."""
     # sign the payload with server's private key
     header = {"typ": "JWS", "alg": jws_signing_alg}
     signed_token = jwt.JWT(header=header, claims=payload)
-    signed_token.make_signed_token(server_private_key)
+    signed_token.make_signed_token(server_private_key.key)
     signed_payload = signed_token.serialize()
     # encrypt the signed payload with client's public key
     header = {
@@ -68,5 +70,5 @@ def make_token(
         "enc": jwe_cek_enc,
     }
     encrypted_token = jwt.JWT(header=header, claims=signed_payload)
-    encrypted_token.make_encrypted_token(client_public_key)
+    encrypted_token.make_encrypted_token(client_public_key.key)
     return encrypted_token.serialize()
