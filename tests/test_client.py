53ca325879de09eaf2cc8b38fd3aa7161b470761
Angela Tran <angela@compiler.la>
2022-05-31 19:29:57 +0000

    test: add stubs for Client unit tests

---
+++ tests/test_client.py
@@ -0,0 +1,30 @@
+from eligibility_api.client import Client
+
+
+def test_create_valid_client():
+    Client
+    pass
+
+
+def test_create_invalid_client_bad_headers():
+    pass
+
+
+def test_client_verify_success():
+    pass
+
+
+def test_client_verify_unexpected_response_code():
+    pass
+
+
+def test_client_verify_failed_request():  # should be parameterized
+    pass
+
+
+def test_client_verify_failed_tokenize_request():
+    pass
+
+
+def test_client_verify_failed_tokenize_response():
+    pass
