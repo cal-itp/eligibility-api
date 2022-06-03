67186fdcbe4a1d5a63b65d6902e5517103ae6418
Angela Tran <angela@compiler.la>
2022-06-03 21:43:15 +0000

    chore: group and move helper functions to be lower in the code

--- tests/test_client.py
+++ tests/test_client.py
@@ -6,6 +6,112 @@ import responses
 from eligibility_api.client import ApiError, Client, TokenError
 
 
+def test_create_valid_client():
+    # Creating a valid Client should not throw an Exception
+    try:
+        Client(**_valid_configuration())
+    except Exception:
+        pytest.fail("Failed to create valid Client")
+
+
+@pytest.mark.parametrize("header_name", ["Authorization", "authorization", "AuThOrIzAtIoN"])
+def test_create_invalid_client_bad_headers(header_name):
+    headers = {header_name: "value"}
+
+    with pytest.raises(ValueError):
+        Client(**_valid_configuration(), headers=headers)
+
+
+@responses.activate
+@pytest.mark.parametrize("status", [200, 400])  # API spec has 400 as an expected code
+def test_client_verify_success(mocker, status):
+    client = Client(**_valid_configuration())
+    mock_request_token(mocker, client)
+    mock_response_token(mocker, client)
+    mock_server_response(status=status)
+
+    # Calling verify with a successful server response should not throw an Exception
+    try:
+        client.verify(*_test_data())
+    except Exception:
+        pytest.fail("Failed to return from Client.verify")
+
+
+@responses.activate
+def test_create_valid_client_additional_headers(mocker):
+    mock_server_response()
+    # Creating a valid client with valid additional headers should not throw an Exception
+    headers = {"X-Server-API-Key": "server-auth-token"}
+    try:
+        client = Client(**_valid_configuration(), headers=headers)
+    except Exception:
+        pytest.fail("Failed to create valid Client")
+
+    mock_request_token(mocker, client)
+    mock_response_token(mocker, client)
+
+    # Calling verify with a successful server response and valid additional headers
+    # should not throw an Exception
+    try:
+        client.verify(*_test_data())
+    except Exception:
+        pytest.fail("Failed to return from Client.verify")
+
+
+@responses.activate
+@pytest.mark.parametrize("status", [403, 404, 500])
+def test_client_verify_unexpected_response_code(mocker, status):
+    client = Client(**_valid_configuration())
+    mock_request_token(mocker, client)
+    mock_response_token(mocker, client)
+    mock_server_response(status=status)
+
+    with pytest.raises(ApiError, match="Unexpected eligibility verification response"):
+        client.verify(*_test_data())
+
+
+@responses.activate
+@pytest.mark.parametrize(
+    "expected_exception,match",
+    [
+        (requests.ConnectionError, "Connection to verification server failed"),
+        (requests.Timeout, "Connection to verification server timed out"),
+        (requests.TooManyRedirects, "Too many redirects to verification server"),
+        (requests.HTTPError, ""),
+    ],
+)
+def test_client_verify_failed_request(mocker, expected_exception, match):
+    client = Client(**_valid_configuration())
+    mock_request_token(mocker, client)
+    mock_response_token(mocker, client)
+    mock_server_error(exception=expected_exception)
+
+    with pytest.raises(ApiError, match=match):
+        client.verify(*_test_data())
+
+
+@responses.activate
+def test_client_verify_failed_tokenize_request(mocker):
+    client = Client(**_valid_configuration())
+    mock_request_token(mocker, client, exception=jwcrypto.JWException)
+    mock_response_token(mocker, client)
+    mock_server_response()
+
+    with pytest.raises(TokenError, match="Failed to tokenize form values"):
+        client.verify(*_test_data())
+
+
+@responses.activate
+def test_client_verify_failed_tokenize_response(mocker):
+    client = Client(**_valid_configuration())
+    mock_request_token(mocker, client)
+    mock_response_token(mocker, client, exception=TokenError)
+    mock_server_response()
+
+    with pytest.raises(TokenError):
+        client.verify(*_test_data())
+
+
 def _test_data():
     return ("A1234567", "Garcia", ["type1"])
 
@@ -69,24 +175,13 @@ def _valid_configuration():
     )
 
 
-def test_create_valid_client():
-    # Creating a valid Client should not throw an Exception
-    try:
-        Client(**_valid_configuration())
-    except Exception:
-        pytest.fail("Failed to create valid Client")
-
-
-@pytest.mark.parametrize("header_name", ["Authorization", "authorization", "AuThOrIzAtIoN"])
-def test_create_invalid_client_bad_headers(header_name):
-    headers = {header_name: "value"}
-
-    with pytest.raises(ValueError):
-        Client(**_valid_configuration(), headers=headers)
-
-
 def mock_server_response(method="GET", url="http://localhost/verify", status=200):
     response = responses.Response(method=method, url=url, status=status)
+    responses.add(response)
+
+
+def mock_server_error(method="GET", url="http://localhost/verify", exception=RuntimeError):
+    response = responses.Response(method=method, url=url, body=exception())
     responses.add(response)
 
 
@@ -104,98 +199,3 @@ def mock_response_token(mocker, client, exception=None):
         mocker.patch.object(client, "_tokenize_response", return_value=mock_response_token)
     else:
         mocker.patch.object(client, "_tokenize_request", side_effect=exception())
-
-
-@responses.activate
-@pytest.mark.parametrize("status", [200, 400])  # API spec has 400 as an expected code
-def test_client_verify_success(mocker, status):
-    client = Client(**_valid_configuration())
-    mock_request_token(mocker, client)
-    mock_response_token(mocker, client)
-    mock_server_response(status=status)
-
-    # Calling verify with a successful server response should not throw an Exception
-    try:
-        client.verify(*_test_data())
-    except Exception:
-        pytest.fail("Failed to return from Client.verify")
-
-
-@responses.activate
-def test_create_valid_client_additional_headers(mocker):
-    mock_server_response()
-    # Creating a valid client with valid additional headers should not throw an Exception
-    headers = {"X-Server-API-Key": "server-auth-token"}
-    try:
-        client = Client(**_valid_configuration(), headers=headers)
-    except Exception:
-        pytest.fail("Failed to create valid Client")
-
-    mock_request_token(mocker, client)
-    mock_response_token(mocker, client)
-
-    # Calling verify with a successful server response and valid additional headers
-    # should not throw an Exception
-    try:
-        client.verify(*_test_data())
-    except Exception:
-        pytest.fail("Failed to return from Client.verify")
-
-
-@responses.activate
-@pytest.mark.parametrize("status", [403, 404, 500])
-def test_client_verify_unexpected_response_code(mocker, status):
-    client = Client(**_valid_configuration())
-    mock_request_token(mocker, client)
-    mock_response_token(mocker, client)
-    mock_server_response(status=status)
-
-    with pytest.raises(ApiError, match="Unexpected eligibility verification response"):
-        client.verify(*_test_data())
-
-
-def mock_server_error(method="GET", url="http://localhost/verify", exception=RuntimeError):
-    response = responses.Response(method=method, url=url, body=exception())
-    responses.add(response)
-
-
-@responses.activate
-@pytest.mark.parametrize(
-    "expected_exception,match",
-    [
-        (requests.ConnectionError, "Connection to verification server failed"),
-        (requests.Timeout, "Connection to verification server timed out"),
-        (requests.TooManyRedirects, "Too many redirects to verification server"),
-        (requests.HTTPError, ""),
-    ],
-)
-def test_client_verify_failed_request(mocker, expected_exception, match):
-    client = Client(**_valid_configuration())
-    mock_request_token(mocker, client)
-    mock_response_token(mocker, client)
-    mock_server_error(exception=expected_exception)
-
-    with pytest.raises(ApiError, match=match):
-        client.verify(*_test_data())
-
-
-@responses.activate
-def test_client_verify_failed_tokenize_request(mocker):
-    client = Client(**_valid_configuration())
-    mock_request_token(mocker, client, exception=jwcrypto.JWException)
-    mock_response_token(mocker, client)
-    mock_server_response()
-
-    with pytest.raises(TokenError, match="Failed to tokenize form values"):
-        client.verify(*_test_data())
-
-
-@responses.activate
-def test_client_verify_failed_tokenize_response(mocker):
-    client = Client(**_valid_configuration())
-    mock_request_token(mocker, client)
-    mock_response_token(mocker, client, exception=TokenError)
-    mock_server_response()
-
-    with pytest.raises(TokenError):
-        client.verify(*_test_data())
